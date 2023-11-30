dd42d12650ab3a1292a26431df84d4d451e194f7
harsh verma <hv73harsh@gmail.com>
2023-11-30 12:55:23 +0530

    first commit

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/hospital", { useNewUrlParser: true, useUnifiedTopology: true  })
+const url = "mongodb://bp32395:swcRWyDk3LMTCcuT@ac-2d40emt-shard-00-00.513bvu4.mongodb.net:27017,ac-2d40emt-shard-00-01.513bvu4.mongodb.net:27017,ac-2d40emt-shard-00-02.513bvu4.mongodb.net:27017/?ssl=true&replicaSet=atlas-6rxsn3-shard-0&authSource=admin&retryWrites=true&w=majority"
+mongoose.connect(url,{useNewUrlParser: true})
 .then(()=>console.log("connect"))
 .catch((error)=>console.log(error));
+ 
